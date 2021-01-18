b00444e576009250fc641a358aa6a3cf10d729f6
Abhishekh Baskaran <abhishekhb@yahoo.co.uk>
2021-01-18 11:27:07 +0000

    Added the env reading code and refactoring az copy data from s3

--- researchanddevelopment/config/env.py
+++ researchanddevelopment/config/env.py
@@ -1,6 +1,7 @@
 from environs import Env
 
-env = Env()
-env.read_env()
+environment_loader = Env()
+environment_loader.read_env()
 
-DEVICE_CONNECTION_STRING = env("DEVICE_CONNECTION_STRING")
+DEVICE_CONNECTION_STRING = environment_loader("DEVICE_CONNECTION_STRING")
+STORAGE_SAS_KEY = environment_loader("STORAGE_ACCOUNT_SAS_KEY")
