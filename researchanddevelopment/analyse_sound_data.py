d9cf5138337a2a872af476b7e89f2a546c71609c
Abhishekh Baskaran <abhishekhb@yahoo.co.uk>
2021-01-30 10:19:13 +0000

    Fixed the plotting of the spectrogram to get the 20Hz frequencies isolated

--- researchanddevelopment/analyse_sound_data.py
+++ researchanddevelopment/analyse_sound_data.py
@@ -6,9 +6,13 @@ from matplotlib import pyplot as plt
 from pydub import AudioSegment
 import scipy
 from scipy.signal import butter, filtfilt
+from noisereduce.utils import int16_to_float32
 
 
 import noisereduce as nr
+
+n_fft = 2048
+hop_length = 512
 
 
 def crop_file(start_sec, end_sec):
@@ -18,49 +22,11 @@ def crop_file(start_sec, end_sec):
     extract.export('metadata/nn01a_20180126_000000_cropped.wav')
 
 
-def fftnoise(f):
-    f = np.array(f, dtype="complex")
-    Np = (len(f) - 1) // 2
-    phases = np.random.rand(Np) * 2 * np.pi
-    phases = np.cos(phases) + 1j * np.sin(phases)
-    f[1 : Np + 1] *= phases
-    f[-1 : -1 - Np : -1] = np.conj(f[1 : Np + 1])
-    return np.fft.ifft(f).real
-
-def band_limited_noise(min_freq, max_freq, samples=1024, samplerate=1):
-    freqs = np.abs(np.fft.fftfreq(samples, 1 / samplerate))
-    f = np.zeros(samples)
-    f[np.logical_and(freqs >= min_freq, freqs <= max_freq)] = 1
-    return fftnoise(f)
-
-
-def spectrogram_data(signal, sr):
-    n_fft = 2048
-    hop_length = 512
-
-    count = 0
-    #dir_path = 'segments/train/data/'
-    #for file in os.listdir(dir_path):
-    # file_path = dir_path + file
-
-    stft = librosa.core.stft(signal, hop_length=hop_length, n_fft=n_fft)
-
-    spectrogram = np.abs(stft)
-    log_spectrogram = librosa.amplitude_to_db(spectrogram)
-    librosa.display.specshow(log_spectrogram, sr=sr, hop_length=hop_length)
-    plt.xlabel('time')
-    plt.ylabel('frequency')
-    plt.colorbar()
-    plt.show()
-    #if count == 100:
-    #    break
-    # count += 1
-
-
 def load_data():
-    # signal, sr = librosa.load('metadata/nn01a_20180126_000000_cropped.wav', sr=22050)
     # Keeping audio at original sample rate
-    signal, sr = librosa.load('metadata/nn01a_20180126_000000_cropped.wav', sr=None)
+    #file_name = 'metadata/nn01a_20180126_000000_cropped.wav'
+    file_name = 'segments/train/data/nn01a_20180126_000000.wav_segment_2_nan.wav'
+    signal, sr = librosa.load(file_name, sr=None)
     print('Duration of samples {}s'.format(len(signal)/sr))
     return signal, sr
 
@@ -82,6 +48,7 @@ def fft_plot(audio, sampling_rate):
     plt.grid()
     plt.xlabel('Frequency -->')
     plt.ylabel('Magnitude')
+    plt.xlim([0,100])
     return plt.show()
 
 
@@ -102,10 +69,10 @@ def butter_lowpass_filter(data, cutoff, nyq, order, time):
     normalized_cutoff = cutoff / nyq
     numerator_coeffs, denominator_coeffs = scipy.signal.butter(order, normalized_cutoff, btype='low', analog=False)
     filtered_signal = scipy.signal.lfilter(numerator_coeffs, denominator_coeffs, data)
-    plt.plot(time, input_signal, 'b-', label = 'signal')
-    plt.plot(time, filtered_signal, 'g-', linewidth = 2, label = 'filtered signal')
-    plt.legend()
-    plt.show()
+    # plt.plot(time, input_signal, 'b-', label = 'signal')
+    # plt.plot(time, filtered_signal, 'g-', linewidth = 2, label = 'filtered signal')
+    # plt.legend()
+    # plt.show()
     return filtered_signal
 
 
@@ -113,21 +80,19 @@ def butter_highpass_filter(data, cutoff, nyq, order, time):
     normalized_cutoff = cutoff / nyq
     numerator_coeffs, denominator_coeffs = scipy.signal.butter(order, normalized_cutoff, btype='high', analog=False)
     filtered_signal = scipy.signal.filtfilt(numerator_coeffs, denominator_coeffs, data)
-    plt.plot(time, input_signal, 'b-', label = 'signal')
-    plt.plot(time, filtered_signal, 'g-', linewidth = 2, label = 'filtered signal')
-    plt.legend()
-    plt.show()
+    # plt.plot(time, input_signal, 'b-', label = 'signal')
+    # plt.plot(time, filtered_signal, 'g-', linewidth = 2, label = 'filtered signal')
+    # plt.legend()
+    # plt.show()
     return filtered_signal
 
 
 def plot_spectrogram(input_data):
-    n_fft = 2048
-    hop_length = 512
     stft = librosa.core.stft(input_data, hop_length=hop_length, n_fft=n_fft)
     spectrogram = np.abs(stft)
-    log_spectrogram = librosa.amplitude_to_db(spectrogram)
-    librosa.display.specshow(
-        log_spectrogram, sr=sr, hop_length=hop_length, x_axis = 's', y_axis = 'log')
+    # log_spectrogram = librosa.amplitude_to_db(spectrogram)
+    librosa.display.specshow(spectrogram, sr=sr, hop_length=hop_length, x_axis = 's', y_axis = 'linear')
+    plt.ylim([0,100])
     plt.xlabel('time')
     plt.ylabel('frequency')
     plt.colorbar()
@@ -136,38 +101,42 @@ def plot_spectrogram(input_data):
 
 def plot_mel(input_data):
     n_mels = 128
-    n_fft = 2048
-    hop_length = 512
     S = librosa.feature.melspectrogram(input_data, sr = sr, n_fft = n_fft, hop_length = hop_length,
                                        n_mels = n_mels)
     S_DB = librosa.power_to_db(S, ref = np.max)
     librosa.display.specshow(S_DB, sr = sr, hop_length = hop_length, x_axis = 'time',
                              y_axis = 'mel')
     plt.colorbar(format = '%+2.0f dB')
+    plt.ylim([0,100])
     plt.show()
+
+def noise_reduce_and_plot(signal):
+    noise_clip = signal[0:duration*1000]
+    reduced_noise = nr.reduce_noise(
+        signal, noise_clip=noise_clip, verbose=False, n_grad_freq=3, hop_length=512, win_length=1024
+    )
+    plot_spectrogram(reduced_noise)
+    plot_mel(reduced_noise)
+    fft_plot(reduced_noise, sr)
+    return reduced_noise
 
 
 if __name__ == "__main__":
     # .wav is lossless
     # crop_file(48860*1000, 49000*1000)
-    # spectrogram_data()
     input_signal, sr = load_data()
-    plot_amp_time(input_signal, sr)
+    # plot_amp_time(input_signal, sr)
     duration = int(len(input_signal)/sr)
     # plots upto sampling rate/2(Nyquist theorem)
     # Filter requirements.
     fs = sr  # sample rate, Hz
-    cutoff = 100  # desired cutoff frequency of the filter, Hz
+    cutoff = 30  # desired cutoff frequency of the filter, Hz
     nyq = 0.5 * fs  # Nyquist Frequency
-    order = 2  # sin wave can be approx represented as quadratic
+    order = 4  # sin wave can be approx represented as quadratic
     time = np.linspace(0, duration, len(input_signal), endpoint = False)
     lowpass_signal = butter_lowpass_filter(input_signal, cutoff, nyq, order, time)
     cutoff_high = 10
     highpass_signal = butter_highpass_filter(lowpass_signal, cutoff_high, nyq, order, time)
+    plot_spectrogram(highpass_signal)
     fft_plot(highpass_signal, sr)
-    noise_clip = highpass_signal[0:duration*1000]
-    reduced_noise = nr.reduce_noise(
-        highpass_signal, noise_clip=noise_clip, verbose=True, n_grad_freq=3
-    )
-    plot_spectrogram(reduced_noise)
-    plot_mel(reduced_noise)
+    noise_reduce_and_plot(noise_reduce_and_plot(highpass_signal))
