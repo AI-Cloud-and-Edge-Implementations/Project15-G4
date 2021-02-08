5ded5692edbbd4330f932e9605080bbdc6a0df24
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-02-08 23:00:44 +0000

    Added commands and refactoring the s3 and azure interface code

--- elephantcallscounter/__main__.py
+++ elephantcallscounter/__main__.py
@@ -1,15 +1,44 @@
 #!/usr/bin/env python
 import os
 
+import click
+
 from elephantcallscounter.data_analysis.analyse_sound_data import AnalyseSoundData
 from elephantcallscounter.data_processing.segment_files import ready_file_segments
+from elephantcallscounter.data_import.import_data import read_from_s3
 
-if __name__ == "__main__":
-    ready_file_segments()
+
+@click.group()
+def entry_point():
+    pass
+
+
+@entry_point.command('analyse_audio_data')
+def analyse_audio_data():
     analyse_sound_data = AnalyseSoundData(
-        file_read_location = os.path.join(os.getcwd(), 'data/metadata/nn01a_20180126_000000_cropped.wav'),
-        save_image_location = os.path.join(os.getcwd(), 'data/spectrogram_images/'),
+        file_read_location = os.path.join(
+            os.getcwd(), 'data/metadata/nn01a_20180126_000000_cropped.wav'
+        ),
+        save_image_location = os.path.join(
+            os.getcwd(), 'data/spectrogram_images/'
+        ),
         sr = 1000,
         hop_length = 256
     )
     analyse_sound_data.analyse_audio()
+
+
+@entry_point.command('generate_file_segments')
+def generate_file_segments():
+    ready_file_segments()
+
+
+@entry_point.command('import_data_from_s3')
+def import_data_from_s3():
+    read_from_s3()
+
+
+if __name__ == "__main__":
+    entry_point.add_command(analyse_audio_data)
+    entry_point.add_command(generate_file_segments)
+    entry_point()
