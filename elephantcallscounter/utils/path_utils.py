5670cd202f711234ef5bedec9dc7ea5b81ba5ce7
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-02-09 22:12:24 +0000

    Added util code

--- elephantcallscounter/utils/path_utils.py
+++ elephantcallscounter/utils/path_utils.py
@@ -1,4 +1,3 @@
-import csv
 from pathlib import Path
 
 
@@ -7,15 +6,3 @@ def get_project_root():
     :return string:
     """
     return Path(__file__).parent.parent
-
-
-def write_to_csv(data, file_name):
-    """ Function to write data to csv file.
-    :param list data:
-    :param string file_name:
-    :return void:
-    """
-    with open(file_name, 'w') as csv_file:
-        writer = csv.writer(csv_file, delimiter = ',')
-        for line in data:
-            writer.writerow(line)
