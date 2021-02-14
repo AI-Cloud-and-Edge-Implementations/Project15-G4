a3b6c0a3b06db963435cb934d4bb365c7068dd3b
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-02-14 12:45:24 +0000

    Added clear segments code

--- elephantcallscounter/data_processing/segment_files.py
+++ elephantcallscounter/data_processing/segment_files.py
@@ -16,6 +16,9 @@ class SegmentFiles:
         """
         self.file_range = file_range
         self.az_importer = az_importer
+        self.start_fresh = True
+        self.training_set = os.path.join(get_project_root(), 'data', 'segments', 'TrainingSet')
+        self.crop_set = os.path.join(get_project_root(), 'data', 'segments', 'CroppedTrainingSet')
 
     @staticmethod
     def generate_file_name(actual_file, start_time, end_time, extension):
@@ -68,49 +71,54 @@ class SegmentFiles:
 
         return files_to_crop
 
+    def crop_files(self, folder_files, files_to_delete):
+        for file_data in folder_files:
+            original_file = os.path.join(
+                self.training_set, file_data[2]
+            )
+            cropped_file = os.path.join(
+                self.crop_set, file_data[3]
+            )
+            AudioProcessing.crop_file(
+                file_data[0],
+                file_data[1],
+                file_name = original_file,
+                destination_file = cropped_file
+            )
+            print('Cropped: ', original_file)
+
+        # remove local file
+        for file_to_remove in os.listdir(files_to_delete):
+            os.remove(os.path.join(files_to_delete, file_to_remove))
+            print("File removed: ", file_to_remove)
+
+    def clear_segments(self):
+        for folder in os.listdir(self.training_set):
+            for file in os.listdir(os.path.join(self.training_set, folder)):
+                os.remove(file)
+        for folder in os.listdir(self.crop_set):
+            for file in os.listdir(os.path.join(self.training_set, folder)):
+                os.remove(file)
+
     def process_segments(self, files_to_crop):
         folder_based_grouping = defaultdict(list)
         for file_data in files_to_crop:
             folder_name = file_data[2].split('/')[0]
             folder_based_grouping[folder_name].append(file_data)
 
+        if self.start_fresh:
+            self.clear_segments()
+
         for folder_name, folder_files in folder_based_grouping.items():
             source_folder = os.path.join('TrainingSet', folder_name)
-            dest_folder = os.path.join(
-                get_project_root(), 'data', 'segments', 'TrainingSet', folder_name
-            )
+            dest_folder = os.path.join(self.training_set, folder_name)
             os.makedirs(dest_folder, exist_ok = True)
             print(f'Processing {source_folder}...')
-            """
             p1 = self.az_importer.az_download_data_from_blob(
                 source_path = source_folder,
                 destination_path = dest_folder
             )
-            """
             print(f'Processing {source_folder} finished!')
-            training_set = os.path.join(get_project_root(), 'data', 'segments', 'TrainingSet')
-            crop_set = os.path.join(get_project_root(), 'data', 'segments', 'CroppedTrainingSet')
-            os.makedirs(os.path.join(crop_set, folder_name), exist_ok = True)
-            files_to_delete = os.path.join(training_set, folder_name)
-
-            for file_data in folder_files:
-                original_file = os.path.join(
-                     training_set, file_data[2]
-                )
-                cropped_file = os.path.join(
-                    crop_set, file_data[3]
-                )
-                """
-                AudioProcessing.crop_file(
-                    file_data[0],
-                    file_data[1],
-                    file_name = original_file,
-                    destination_file = cropped_file
-                )
-                """
-                print('Cropped: ', original_file)
-
-            # remove local file
-            for file_to_remove in os.listdir(files_to_delete):
-                os.remove(os.path.join(files_to_delete, file_to_remove))
-                print("File removed: ", file_to_remove)
+            os.makedirs(os.path.join(self.crop_set, folder_name), exist_ok = True)
+            files_to_delete = os.path.join(self.training_set, folder_name)
+            self.crop_files(folder_files, files_to_delete)
