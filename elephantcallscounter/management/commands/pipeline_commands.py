bf612c8c2647b04101bdda9cb92eeae723bf52a4
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-03-05 22:58:00 +0000

    Fixed the pipeline command for the argument

--- elephantcallscounter/management/commands/pipeline_commands.py
+++ elephantcallscounter/management/commands/pipeline_commands.py
@@ -7,7 +7,7 @@ demo = Blueprint('demo', __name__)
 
 
 @demo.cli.command('run_demo')
-@demo.cli.argument('file_path')
+@click.argument('file_path')
 @click.pass_context
 def run_demo(context, file_path):
     pipeline_run(file_path = file_path)
