d8e0be83db4036998fa3e850d99cd0b410a1d412
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-03-06 10:31:05 +0000

    Added test fixtures

--- elephantcallscounter/tests/test_pipeline_services.py
+++ elephantcallscounter/tests/test_pipeline_services.py
@@ -3,4 +3,6 @@ from elephantcallscounter.services.pipeline_services import pipeline_run
 
 class TestPipelineServices:
     def test_run_pipeline(self):
-        assert pipeline_run('data/nn01a_20180126_000000_48835100.0_48895100.0_cropped.wav') == 1
+        assert pipeline_run(
+            'tests/test_fixtures/nn01a_20180126_000000_48835100.0_48895100.0_cropped.wav'
+        ) == 1
