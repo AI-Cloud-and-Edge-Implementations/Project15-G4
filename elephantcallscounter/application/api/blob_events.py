29649ee96561c4d2e510261f993ed485265aabe2
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-03-06 16:02:12 +0000

    Got the front end visualization setup

--- elephantcallscounter/application/api/blob_events.py
+++ elephantcallscounter/application/api/blob_events.py
@@ -5,6 +5,8 @@ from elephantcallscounter.adapters.azure_interface import AzureInterface
 from elephantcallscounter.services.pipeline_services import pipeline_run
 from elephantcallscounter.utils.path_utils import join_paths
 from elephantcallscounter.utils.path_utils import get_project_root
+from elephantcallscounter.common.constants import RUN_FRESH
+from elephantcallscounter.application.persistence.models.elephants import delete_all_elephants
 
 blob_blueprint = Blueprint(
     'blob_events',
@@ -15,6 +17,8 @@ blob_blueprint = Blueprint(
 
 @blob_blueprint.route('/run_pipeline/', methods = ['GET'])
 def run_processing():
+    if RUN_FRESH:
+        delete_all_elephants()
     queue_name = request.args.get('queue_name')
     container_name = request.args.get('container_name')
     audio_events_queue = AudioEventsQueue(queue_name)
@@ -28,4 +32,4 @@ def run_processing():
             dest_file = join_paths([get_project_root(), file_path])
         )
         print('about to run pipeline on {}!'.format(file_path))
-        pipeline_run(file_path, 'data/labels/spec_images_labels')
+        pipeline_run(file_path, 'data/labels/spec_images_labels.csv')
--- elephantcallscounter/application/api/elephants_view.py
+++ elephantcallscounter/application/api/elephants_view.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 
 from flask import Blueprint
+from flask import current_app
 from flask import render_template
 from flask import request
+from flask_googlemaps import Map
+from flask_googlemaps import GoogleMaps
 
-from elephantcallscounter.app_factory import db
+from elephantcallscounter import db
 from elephantcallscounter.application.persistence.models.elephants import Elephants
+from elephantcallscounter.config import env
 from elephantcallscounter.utils.path_utils import get_project_root
 from elephantcallscounter.utils.path_utils import join_paths
+
 
 template_folder_loc = join_paths([get_project_root(), 'app/templates'])
 
@@ -17,6 +22,9 @@ elephant_blueprint = Blueprint(
     url_prefix = '/elephants',
     template_folder = template_folder_loc
 )
+
+
+GoogleMaps(current_app, key = env.GOOGLE_API_KEY)
 
 
 @elephant_blueprint.route('/elephants_count/')
@@ -32,9 +40,82 @@ def elephant_counter():
     ).all()
     print(start_time)
     print(end_time)
-    print('Number of elephants', elephants)
 
-    return {'Number of elephants': len(elephants)}
+    json_data = { # for testing
+        'user' : {
+            'x' : 37.50611,
+            'y' : 127.0616346
+        },
+        'devices' : [
+            {
+                'id' : '0001',
+                'x' : 37.5077121,
+                'y' : 127.0624397,
+                'data' : 'something'
+            }
+        ]
+    }
+    devices_data = {}  # dict to store data of devices
+    devices_location = {}  # dict to store coordinates of devices
+
+    user_location = (json_data['user']['x'], json_data['user']['y'])
+    # json example : { 'user' : { 'x' : '300' , 'y' : '300' } }
+    # get user_location from json & store as turple (x, y)
+
+    devices_data[str(json_data['devices'][0]['id'])] = (
+        json_data['devices'][0]['data']
+    )
+
+    devices_location[str(json_data['devices'][0]['id'])] = (
+        json_data['devices'][0]['x'],
+        json_data['devices'][0]['y']
+    )
+    # json example : { 'devices' : { 'id' : '0001', x' : '500', 'y' : '500' }, { ... } }
+    # get device_location from json & store turple (x, y) in dictionary with device id as key
+    # use for statements or something to get more locations from more devices
+
+    circle = { # draw circle on map (user_location as center)
+        'stroke_color': '#0000FF',
+        'stroke_opacity': .5,
+        'stroke_weight': 5,
+        # line(stroke) style
+        'fill_color': '#FFFFFF',
+        'fill_opacity': .2,
+        # fill style
+        'center': { # set circle to user_location
+            'lat': user_location[0],
+            'lng': user_location[1]
+        },
+        'radius': 500 # circle size (50 meters)
+    }
+
+    map = Map(
+        identifier = "map", varname = "map",
+        # set identifier, varname
+        lat = user_location[0], lng = user_location[1],
+        # set map base to user_location
+        zoom = 15, # set zoomlevel
+        markers = [
+            {
+                'lat': devices_location['0001'][0],
+                'lng': devices_location['0001'][1],
+                'infobox': devices_data['0001']
+            }
+        ],
+        # set markers to location of devices
+        circles = [circle] # pass circles
+    )
+
+    return render_template(
+        'index.html',
+        map=map,
+        locations_data=[
+            {'lat': 0.0236, 'lng': 37.9062},
+            {'lat': 0.08, 'lng': 38.9062},
+            {'lat': 1.5, 'lng': 39.9062}
+        ],
+        labels = [0, 1, 2]
+    )
 
 
 @elephant_blueprint.route('/add_elephant_count/')
