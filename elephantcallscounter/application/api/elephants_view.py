b8a5af78e71c496ac5f4725aa1ecd09f17f8655a
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-03-06 11:43:22 +0000

    Altered flask api to return the number of elephants found

--- elephantcallscounter/application/api/elephants_view.py
+++ elephantcallscounter/application/api/elephants_view.py
@@ -19,15 +19,41 @@ elephant_blueprint = Blueprint(
 )
 
 
-@elephant_blueprint.route('/elephants_count/<string:start_time>/<string:end_time>/')
-def elephant_counter(start_time, end_time):
-    if request.method == 'GET':
-        start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
-        elephants = db.session.query(Elephants).filter(
-            start_time >= Elephants.start_time
-        ).filter(
-            end_time <= Elephants.end_time
-        ).all()
-        print('Number of elephants', elephants)
+@elephant_blueprint.route('/elephants_count/')
+def elephant_counter():
+    start_time = request.args.get('start_time')
+    end_time = request.args.get('end_time')
+    start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
+    end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
+    elephants = db.session.query(Elephants).filter(
+        start_time >= Elephants.start_time
+    ).filter(
+        end_time <= Elephants.end_time
+    ).all()
+    print(start_time)
+    print(end_time)
+    print('Number of elephants', elephants)
 
-    return render_template(join_paths([template_folder_loc, 'index.html']))
+    return {'Number of elephants': len(elephants)}
+
+
+@elephant_blueprint.route('/add_elephant_count/')
+def add_elephant_count():
+    start_time = datetime.strptime(
+        request.args.get('start_time'), '%Y-%m-%d %H:%M:%S'
+    )
+    end_time = datetime.strptime(
+        request.args.get('end_time'), '%Y-%m-%d %H:%M:%S'
+    )
+    new_elephant = Elephants(
+        latitude = float(request.args.get('latitude')),
+        longitude = float(request.args.get('longitude')),
+        start_time = start_time,
+        end_time = end_time,
+        device_id = int(request.args.get('device_id')),
+        number_of_elephants = int(request.args.get('number_of_elephants'))
+    )
+    db.session.add(new_elephant)
+    db.session.commit()
+
+    return {'message': 'new elephant added'}
--- elephantcallscounter/application/persistence/models/elephants.py
+++ elephantcallscounter/application/persistence/models/elephants.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, DateTime, Integer, Float
 class Elephants(db.Model):
     __tablename__ = 'elephants'
 
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key = True, autoincrement = True)
     latitude = Column(Float, default=0.0)
     longitude = Column(Float, default=0.0)
     start_time = Column(DateTime)
