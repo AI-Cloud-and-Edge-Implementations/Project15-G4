81308059502766395df1bd8710fc6e948ee98bad
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-03-06 19:06:20 +0000

    Fixed the device id issue that was causing the parsing to fail

--- elephantcallscounter/application/api/elephants_view.py
+++ elephantcallscounter/application/api/elephants_view.py
@@ -1,18 +1,14 @@
 from datetime import datetime
 
 from flask import Blueprint
-from flask import current_app
 from flask import render_template
 from flask import request
-from flask_googlemaps import Map
-from flask_googlemaps import GoogleMaps
 
 from elephantcallscounter import db
 from elephantcallscounter.application.persistence.models.elephants import Elephants
-from elephantcallscounter.config import env
 from elephantcallscounter.utils.path_utils import get_project_root
 from elephantcallscounter.utils.path_utils import join_paths
-
+from elephantcallscounter.common.constants import LOCATION
 
 template_folder_loc = join_paths([get_project_root(), 'app/templates'])
 
@@ -24,20 +20,20 @@ elephant_blueprint = Blueprint(
 )
 
 
-GoogleMaps(current_app, key = env.GOOGLE_API_KEY)
-
-
 @elephant_blueprint.route('/elephants_count/')
 def elephant_counter():
     start_time = request.args.get('start_time')
     end_time = request.args.get('end_time')
     start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
     end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
-    elephants = db.session.query(Elephants).filter(
-        start_time >= Elephants.start_time
+    """
+    TODO: Add time based aggregation:
+            start_time >= Elephants.start_time
     ).filter(
         end_time <= Elephants.end_time
-    ).all()
+
+    """
+    elephants = db.session.query(Elephants).all()
     print(start_time)
     print(end_time)
     elephant_output = [
@@ -45,18 +41,14 @@ def elephant_counter():
         for elephant in elephants
     ]
     location_data = [
-        
+        LOCATION[elephant.device_id]
+        for elephant in elephants
     ]
-
     return render_template(
         'index.html',
         number_of_elephants = elephant_output,
-        locations_data=[
-            {'lat': 0.0236, 'lng': 37.9062},
-            {'lat': 0.08, 'lng': 38.9062},
-            {'lat': 1.5, 'lng': 39.9062}
-        ],
-        labels = ['nn01', 'nn02', 'nn03']
+        locations_data=location_data,
+        labels = list(LOCATION.keys())
     )
 
 
@@ -73,7 +65,7 @@ def add_elephant_count():
         longitude = float(request.args.get('longitude')),
         start_time = start_time,
         end_time = end_time,
-        device_id = int(request.args.get('device_id')),
+        device_id = request.args.get('device_id'),
         number_of_elephants = int(request.args.get('number_of_elephants'))
     )
     db.session.add(new_elephant)
