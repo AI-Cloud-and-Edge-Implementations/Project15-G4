6aae8278c78451d6b74692b2434251421f722b31
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-03-06 16:42:37 +0000

    Added working html markers and table borders/font size fixes

--- elephantcallscounter/application/api/elephants_view.py
+++ elephantcallscounter/application/api/elephants_view.py
@@ -41,80 +41,19 @@ def elephant_counter():
     print(start_time)
     print(end_time)
 
-    json_data = { # for testing
-        'user' : {
-            'x' : 37.50611,
-            'y' : 127.0616346
-        },
-        'devices' : [
-            {
-                'id' : '0001',
-                'x' : 37.5077121,
-                'y' : 127.0624397,
-                'data' : 'something'
-            }
-        ]
-    }
-    devices_data = {}  # dict to store data of devices
-    devices_location = {}  # dict to store coordinates of devices
-
-    user_location = (json_data['user']['x'], json_data['user']['y'])
-    # json example : { 'user' : { 'x' : '300' , 'y' : '300' } }
-    # get user_location from json & store as turple (x, y)
-
-    devices_data[str(json_data['devices'][0]['id'])] = (
-        json_data['devices'][0]['data']
-    )
-
-    devices_location[str(json_data['devices'][0]['id'])] = (
-        json_data['devices'][0]['x'],
-        json_data['devices'][0]['y']
-    )
-    # json example : { 'devices' : { 'id' : '0001', x' : '500', 'y' : '500' }, { ... } }
-    # get device_location from json & store turple (x, y) in dictionary with device id as key
-    # use for statements or something to get more locations from more devices
-
-    circle = { # draw circle on map (user_location as center)
-        'stroke_color': '#0000FF',
-        'stroke_opacity': .5,
-        'stroke_weight': 5,
-        # line(stroke) style
-        'fill_color': '#FFFFFF',
-        'fill_opacity': .2,
-        # fill style
-        'center': { # set circle to user_location
-            'lat': user_location[0],
-            'lng': user_location[1]
-        },
-        'radius': 500 # circle size (50 meters)
-    }
-
-    map = Map(
-        identifier = "map", varname = "map",
-        # set identifier, varname
-        lat = user_location[0], lng = user_location[1],
-        # set map base to user_location
-        zoom = 15, # set zoomlevel
-        markers = [
-            {
-                'lat': devices_location['0001'][0],
-                'lng': devices_location['0001'][1],
-                'infobox': devices_data['0001']
-            }
-        ],
-        # set markers to location of devices
-        circles = [circle] # pass circles
-    )
-
     return render_template(
         'index.html',
-        map=map,
+        number_of_elephants = [{
+            'nn01': 0,
+            'nn02': 1,
+            'nn03': 2
+        }],
         locations_data=[
             {'lat': 0.0236, 'lng': 37.9062},
             {'lat': 0.08, 'lng': 38.9062},
             {'lat': 1.5, 'lng': 39.9062}
         ],
-        labels = [0, 1, 2]
+        labels = ['nn01', 'nn02', 'nn03']
     )
 
 
