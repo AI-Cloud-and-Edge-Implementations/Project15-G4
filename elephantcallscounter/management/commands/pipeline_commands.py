af9b59f33a3577c74867f19b4bfacf562fb81be9
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-03-07 20:29:50 +0000

    Fixed the pipeline commands

--- elephantcallscounter/management/commands/pipeline_commands.py
+++ elephantcallscounter/management/commands/pipeline_commands.py
@@ -7,7 +7,7 @@ demo = Blueprint('demo', __name__)
 
 
 @demo.cli.command('run_demo')
-@click.argument('file_path')
+@click.argument('folder_path')
 @click.pass_context
-def run_demo(context, file_path):
-    pipeline_run(file_path = file_path, csv_file_path = 'data/labels/spec_images_labels.csv')
+def run_demo(context, folder_path):
+    pipeline_run(folder_path, csv_file_path = 'data/labels/spec_images_labels.csv')
